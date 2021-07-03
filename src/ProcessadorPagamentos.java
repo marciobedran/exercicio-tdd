f61b24d1c6a9a780672e302bec66197b00a327f3
Márcio Bedran <marcio_bedran@hotmail.com>
2021-07-03 19:22:55 -0300

    primeiro teste passando

--- src/ProcessadorPagamentos.java
+++ src/ProcessadorPagamentos.java
@@ -3,7 +3,14 @@ import java.util.ArrayList;
 public class ProcessadorPagamentos {
 	
 	public void processaPagamentos(Fatura fatura, ArrayList<Boleto> boletos) {
-		
+		double valor_total_boletos = 0.0;
+		double valor_total_fatura = fatura.getValorTotal();
+		for (Boleto boleto : boletos) {
+			valor_total_boletos += boleto.getValor();
+		}
+		if (valor_total_boletos >= valor_total_fatura) {
+			fatura.setStatus("PAGA");
+		}
 	}
 	
 }
