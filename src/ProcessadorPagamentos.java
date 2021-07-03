40126b97f40d44c0cd6c9c554a90bd949b708e70
Márcio Bedran <marcio_bedran@hotmail.com>
2021-07-03 19:34:00 -0300

    quinto teste passando

--- src/ProcessadorPagamentos.java
+++ src/ProcessadorPagamentos.java
@@ -3,17 +3,20 @@ import java.util.ArrayList;
 public class ProcessadorPagamentos {
 	
 	public void processaPagamentos(Fatura fatura, ArrayList<Boleto> boletos) {
-		double valor_total_boletos = 0.0;
-		double valor_total_fatura = fatura.getValorTotal();
-		for (Boleto boleto : boletos) {
-			valor_total_boletos += boleto.getValor();
+		if (boletos.size() > 0) {
+			double valor_total_boletos = 0.0;
+			double valor_total_fatura = fatura.getValorTotal();
+			for (Boleto boleto : boletos) {
+				valor_total_boletos += boleto.getValor();
+			}
+			if (valor_total_boletos >= valor_total_fatura) {
+				fatura.setStatus("PAGA");
+			}
+			else {
+				fatura.setStatus("PARCIALMENTE_PAGA");
+			}
 		}
-		if (valor_total_boletos >= valor_total_fatura) {
-			fatura.setStatus("PAGA");
-		}
-		else {
-			fatura.setStatus("PARCIALMENTE_PAGA");
-		}
+		
 	}
 	
 }
