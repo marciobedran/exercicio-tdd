2c7a9c224703420c5f72da429fa0fe13cbebd931
Márcio Bedran <marcio_bedran@hotmail.com>
2021-07-03 19:26:05 -0300

    segundo teste criado e passando
    quando resolvi o primeiro teste acabei utilizando o >= direto e acabou que esse teste "nasceu" passando

--- test/ProcessadorPagamentosTest.java
+++ test/ProcessadorPagamentosTest.java
@@ -23,5 +23,22 @@ class ProcessadorPagamentosTest {
 		processador.processaPagamentos(fatura, boletos);
 		Assertions.assertEquals(fatura.getStatus(), "PAGA");
 	}
+	
+	@Test
+	@DisplayName("Teste com valor do somatorio dos boletos igual ao valor da fatura")
+	void TestaFaturaPagaParaValoresMaiores() {
+
+		ProcessadorPagamentos processador = new ProcessadorPagamentos();
+		Fatura fatura = new Fatura(new Date(), 1500.00, "Marcio Bedran");
+		Boleto boleto1 = new Boleto(1, new Date(), 1000.00);
+		Boleto boleto2 = new Boleto(2, new Date(), 500.00);
+		Boleto boleto3 = new Boleto(3, new Date(), 250.00);
+		ArrayList<Boleto> boletos = new ArrayList<Boleto>();
+		boletos.add(boleto1);
+		boletos.add(boleto2);
+		boletos.add(boleto3);
+		processador.processaPagamentos(fatura, boletos);
+		Assertions.assertEquals(fatura.getStatus(), "PAGA");
+	}
 
 }
